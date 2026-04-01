58dcbd1c6dc4b6e6ffd9e89721eb87fcc137d78d
ricardobatt <ricardob4tist4@gmail.com>
2026-03-31 22:10:51 -0300

    fix(js) Arruma dashboard

--- source/js/dashboardHome.js
+++ source/js/dashboardHome.js
@@ -1,3 +1,5 @@
+const logado = JSON.parse(sessionStorage.getItem("usuarioAtivo"));
+
 if (logado) {
   document.getElementById("nome-usuario").innerText = `Bem-vindo, ${logado.identidade}`;
 } else {
