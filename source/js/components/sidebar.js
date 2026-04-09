41351f282019ac700b957c9b102254600dda90ff
ricardobatt <ricardob4tist4@gmail.com>
2026-04-09 14:28:07 -0300

    feat: Adiciona cor para indicar pagina atual ao sidebar

--- source/js/components/sidebar.js
+++ source/js/components/sidebar.js
@@ -1,112 +1,60 @@
 class SidebarIntegraCat extends HTMLElement {
-  
   connectedCallback() {
-    
     this.innerHTML = `
       <button id="btn-abrir">
         <img src="../source/img/icons/menu.svg" alt="Abrir menu" width="30px">
       </button>
       <nav class="sidebar-nav">
         <div class="title-logo">
-          <img
-            src="../source/img/logo integraCAT-nolet.svg"
-            alt="logo - IntegraCAT"
-          />
-          <img
-            src="../source/img/lettering integraCAT.svg"
-            alt="letreiro - IntegraCAT"
-          />
+          <img src="../source/img/logo integraCAT-nolet.svg" alt="logo - IntegraCAT" />
+          <img src="../source/img/lettering integraCAT.svg" alt="letreiro - IntegraCAT" />
           <button id="btn-fechar">
-            <img
-              src="../source/img/icons/backArrow2.svg"
-              alt="voltar"
-              width="24px"
-            />
+            <img src="../source/img/icons/backArrow2.svg" alt="voltar" width="24px" />
           </button>
         </div>
         <ul>
-          <li>
-            <a href="../pages/dashboard.html">
-              <img
-                src="../source/img/icons/dashboard-icons/dashboard-icon.svg"
-                alt="dashboard-icon"
-              />
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li>
-            <a href="../pages/dashboard-historico.html">
-              <img
-                src="../source/img/icons/dashboard-icons/historic-icon.svg"
-                alt="historico-icon"
-              />
-              <p>Histórico de Ocorrências</p>
-            </a>
-          </li>
-          <li>
-            <a href="../pages/dashboard-criarOcorrencia.html">
-              <img
-                src="../source/img/icons/dashboard-icons/create-icon.svg"
-                alt="criar-icon"
-              />
-              <p>Criar Ocorrências</p>
-            </a>
-          </li>
-          <li>
-            <a href="../pages/dashboard-palestras.html">
-              <img
-                src="../source/img/icons/dashboard-icons/lecture-icon.svg"
-                alt="palestras-icon"
-              />
-              <p>Palestras</p>
-            </a>
-          </li>
-          <li>
-            <a href="../pages/dashboard-conteudos.html">
-              <img
-                src="../source/img/icons/dashboard-icons/content-icon.svg"
-                alt="conteudos-icon"
-              />
-              <p>Conteúdos</p>
-            </a>
-          </li>
-          <li>
-            <a href="../pages/dashboard-configuracoes.html">
-              <img
-                src="../source/img/icons/dashboard-icons/config-icon.svg"
-                alt="configuracaes-icon"
-              />
-              <p>Configurações</p>
-            </a>
-          </li>
-          <li>
-            <a id="btn-sair" onclick="sairDaConta()" href="../index.html">
-              <img
-                src="../source/img/icons/dashboard-icons/leave-icon.svg"
-                alt="sair-icon"
-              />
-              <p>Sair da Conta</p>
-            </a>
-          </li>
+          <li><a href="dashboard.html"><img src="../source/img/icons/dashboard-icons/dashboard-icon.svg" /><p>Dashboard</p></a></li>
+          <li><a href="dashboard-historico.html"><img src="../source/img/icons/dashboard-icons/historic-icon.svg" /><p>Histórico</p></a></li>
+          <li><a href="dashboard-criarOcorrencia.html"><img src="../source/img/icons/dashboard-icons/create-icon.svg" /><p>Criar Ocorrências</p></a></li>
+          <li><a href="dashboard-palestras.html"><img src="../source/img/icons/dashboard-icons/lecture-icon.svg" /><p>Palestras</p></a></li>
+          <li><a href="dashboard-conteudos.html"><img src="../source/img/icons/dashboard-icons/content-icon.svg" /><p>Conteúdos</p></a></li>
+          <li><a href="dashboard-configuracoes.html"><img src="../source/img/icons/dashboard-icons/config-icon.svg" /><p>Configurações</p></a></li>
+          <li><a id="btn-sair" onclick="sairDaConta()" href="../index.html"><img src="../source/img/icons/dashboard-icons/leave-icon.svg" /><p>Sair</p></a></li>
         </ul>
       </nav>
     `;
 
+    this.setActiveLink();
+    this.setupEventListeners();
+  }
+
+  setActiveLink() {
+    const currentPath = window.location.pathname;
+    const links = this.querySelectorAll('ul li a');
+
+    links.forEach(link => {
+      const linkHref = link.getAttribute('href');
+      // Lógica: se o nome do arquivo no href estiver na URL atual
+      if (linkHref && currentPath.includes(linkHref.split('/').pop())) {
+        link.parentElement.classList.add('btn-selecionado');
+      }
+    });
+  }
+
+  setupEventListeners() {
     const sidebar = this.querySelector('.sidebar-nav');
     const btnAbrir = this.querySelector('#btn-abrir');
     const btnFechar = this.querySelector('#btn-fechar');
 
-    if (btnFechar && btnAbrir && sidebar) {
-      btnFechar.addEventListener('click', () => {
-        sidebar.classList.add('escondida');
-        btnAbrir.style.display = 'block';
-      });
+    btnFechar?.addEventListener('click', () => {
+      sidebar.classList.add('escondida');
+      btnAbrir.style.display = 'block';
+    });
 
-      btnAbrir.addEventListener('click', () => {
-        sidebar.classList.remove('escondida');
-        btnAbrir.style.display = 'none';
-      });
-    }
+    btnAbrir?.addEventListener('click', () => {
+      sidebar.classList.remove('escondida');
+      btnAbrir.style.display = 'none';
+    });
   }
 }
 
